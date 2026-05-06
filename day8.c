3c1e93e48e04eb199a543617e165b960ec9ae45e
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2026-05-06 22:12:49 +0530

    Compute Power Using Recursion

---
+++ day8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int power(int a, int b) {
+    // Base case
+    if (b == 0) return 1;
+    
+    // Recursive case
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", power(a, b));
+    return 0;
+}
