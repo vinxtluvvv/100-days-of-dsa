cf069666b4af0fd175691b8836f17c7fb12edbbc
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2026-05-06 22:45:48 +0530

    Count Occurrences of an Element in Linked List

---
+++ day25.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* current = head;
+    
+    while (current != NULL) {
+        if (current->data == key) {
+            count++;
+        }
+        current = current->next;
+    }
+    
+    return count;
+}
+
+int main() {
+    int n, value, key;
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    
+    scanf("%d", &key);
+    printf("%d\n", countOccurrences(head, key));
+    return 0;
+}
