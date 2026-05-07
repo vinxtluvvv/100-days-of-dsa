6a4dc4a55b538aa5caf8fb95283c72a26a8a8e6b
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2026-05-07 20:47:39 +0530

    Graph Representation Using Adjacency List

---
+++ day62.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // For undirected graph
+    }
+    return 0;
+}
