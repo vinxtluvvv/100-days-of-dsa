8cd99f99606e6c152c32fbc3d57c002c8d676206
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2026-05-07 09:02:57 +0530

    Level Order Traversal

---
+++ day46.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
