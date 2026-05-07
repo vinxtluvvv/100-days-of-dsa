727f7eb8ed6f5699d554bfa7ea0b9300a28f5f05
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2026-05-07 21:16:05 +0530

    Counting Sort Implementation

---
+++ day94.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void countingSort(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+
+    int* count = (int*)calloc(max + 1, sizeof(int));
+    int* output = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) count[arr[i]]++;
+
+    for (int i = 1; i <= max; i++) count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) arr[i] = output[i];
+
+    free(count);
+    free(output);
+}
+
+int main() {
+    int arr[] = {4, 2, 2, 8, 3, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    countingSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
